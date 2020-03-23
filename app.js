21ee4b3286d493f0539abf8f21af1be8d2722ccd
Claudio Duarte Rodrigues <cluadio.duarte.98@live.com.pt>
2020-03-23 10:34:33 +0100

    Login and Register

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var bd = require('./connection')
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000
+const authRoute = require('./routes/auth')
+const dotenv = require('dotenv')
 
-app.use('/', require('./routes/index'))
-app.use('/user', require('./routes/user'))
-app.listen(PORT, () => console.log(`Example app listening on port port!: ` + "http://127.0.0.1:3000"))
+dotenv.config();
+
+mongoose.connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => console.log("Connected to DB"));
+
+app.use(express.json())
+
+app.use('/api/user', authRoute)
+app.listen(PORT, () => console.log("Server Up and Running"))
