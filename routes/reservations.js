4926e182fb6495f3d0b92ae1ba3215853ccd8f19
Claudio Duarte Rodrigues <cluadio.duarte.98@live.com.pt>
2020-05-10 04:22:29 +0200

    My reservations route

--- routes/reservations.js
+++ routes/reservations.js
@@ -5,12 +5,16 @@ const router = require('express').Router()
 router.post('/new', async (req, res) => {
     var restaurant_id = req.body.restaurant_id
     var user_id = req.body.user_id
+    var restaurant_name = req.body.restaurant_name
+    var restaurant_address = req.body.restaurant_address
     var number_guests = req.body.number_guests
     var date = req.body.date
     var time = req.body.time
     var data = {
         "user_id" : user_id,
         "restaurant_id" : restaurant_id,
+        "restaurant_name": restaurant_name,
+        "restaurant_address": restaurant_address,
         "date" : date,
         "time" : time,
         "number_guests" : number_guests
@@ -23,6 +27,13 @@ router.post('/new', async (req, res) => {
     else {
         res.send('Reservation made successfully')
     }
+})
+
+router.get('/user', async (req, res) => {
+    var user_id = req.query.user_id
+    
+    await db.find('reservation', 'user_id', user_id)
+    res.send(db.results)
 })
 
 module.exports = router
